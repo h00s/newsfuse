8a0976bf48bd434880dd1cb0b231c46c7b3fe0ad
Krunoslav Husak <kruno@husak.me>
2024-01-09 22:52:26 +0100

    Updated Raptor to v0.2.3

--- backend/config/routes.go
+++ backend/config/routes.go
@@ -3,7 +3,7 @@ package config
 import "github.com/h00s/raptor"
 
 func Routes() raptor.Routes {
-	return raptor.Routes{
+	return raptor.CollectRoutes(
 		raptor.Route("GET", "*", "SPAController", "Index"),
-	}
+	)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,7 +2,7 @@ module github.com/h00s/newsfuse
 
 go 1.21.5
 
-require github.com/h00s/raptor v0.2.2
+require github.com/h00s/raptor v0.2.3
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
