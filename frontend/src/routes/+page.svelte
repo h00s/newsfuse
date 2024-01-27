5fdbfd4a3b2eafa7a0b610d89b4f6b808809cb4c
Krunoslav Husak <kruno@husak.me>
2024-01-27 18:23:52 +0100

    Displaying published datetime

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <script>
   export let data;
+
+  function formatDateTime(date) {
+    date = new Date(date);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}.${month}.${year}. ${hours}:${minutes}`;
+  }
 </script>
 
 <div>
@@ -8,7 +19,7 @@
       <h3 class="text-gray-900 dark:text-white text-lg font-bold">
         <a class="mb-4" href="{headline.URL}">{headline.Title}</a>
       </h3>
-      <span class="text-gray-400 mb-4">{headline.Source}</span>
+      <span class="text-gray-400 mb-4">{formatDateTime(headline.PublishedAt)} | {headline.Source}</span>
     </div>
   {/each}
 </div>
