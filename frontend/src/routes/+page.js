f458a816d2a5ba99acbc2976e4e75893887ece77
Krunoslav Husak <kruno@husak.me>
2024-02-03 09:35:44 +0100

    Removed unnecessary fetch

--- frontend/src/routes/+page.js
+++ frontend/src/routes/+page.js
@@ -3,14 +3,6 @@ import { PUBLIC_API_URL } from '$env/static/public'
 export async function load({ fetch, params }) {
 	let res = await fetch(PUBLIC_API_URL + '/headlines');
 	const headlines = await res.json();
-	
-	res = await fetch(PUBLIC_API_URL + '/sources');
-	const sources = await res.json();
-
-	headlines.forEach(headline => {
-		const source = sources.find(source => source.id === headline.sourceId);
-		headline.Source = source;
-	});
 
 	return {
     headlines: headlines,
