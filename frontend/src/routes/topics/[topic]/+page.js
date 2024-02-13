569a916cf95fe8749b96969e8296e40fc749e823
Krunoslav Husak <kruno@husak.me>
2024-02-13 20:52:58 +0100

    Concurrent topics and headlines fetch

--- frontend/src/routes/topics/[topic]/+page.js
+++ frontend/src/routes/topics/[topic]/+page.js
@@ -1,10 +1,10 @@
 import { PUBLIC_API_URL } from '$env/static/public'
 
 export async function load({ fetch, params }) {
-	let res = await fetch(`${PUBLIC_API_URL}/topics`);
-	const topics = await res.json();
-	res = await fetch(`${PUBLIC_API_URL}/topics/${params.topic}/headlines`);
-	const headlines = await res.json();
+	let [topics, headlines] = await Promise.all([
+    fetch(`${PUBLIC_API_URL}/topics`).then(res => res.json()),
+    fetch(`${PUBLIC_API_URL}/topics/${params.topic}/headlines`).then(res => res.json())
+	]);
 
 	return {
 		topics: topics,
