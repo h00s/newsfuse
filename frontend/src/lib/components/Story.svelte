d545309822bc9af316e0f804b31499d34a9ae54d
Krunoslav Husak <kruno@husak.me>
2024-02-16 10:46:20 +0100

    Finished story summarization on the frontend
    Closes #5

--- frontend/src/lib/components/Story.svelte
+++ frontend/src/lib/components/Story.svelte
@@ -3,6 +3,7 @@
 
   export let headlineId;
   let story;
+  let buttonSummarizeText = "Sažmi članak";
 
   if (story === undefined) {
     fetch(`${PUBLIC_API_URL}/headlines/${headlineId}/story`)
@@ -11,13 +12,28 @@
         story = data;
       });
   }
+
+  function summarizeStory() {
+    buttonSummarizeText = '<span class="loading loading-spinner loading-sm"></span>';
+    fetch(`${PUBLIC_API_URL}/stories/${story.ID}/summarize`)
+    .then((response) => response.json())
+      .then((data) => {
+        story = data;
+        story.Content = story.Summary;
+      });
+  }
 </script>
 
 <div class="pt-4 text-gray-700 dark:text-gray-300">
   <p>
     {#if story === undefined}
-      <span class="loading loading-spinner loading-sm"></span>
+    <span class="loading loading-spinner loading-sm"></span>
     {:else}
+    {#if story.Content.length > 800}
+        <div class="flex justify-center pb-2">
+          <button class="btn btn-sm btn-primary" on:click={summarizeStory}>{@html buttonSummarizeText}</button>
+        </div>
+      {/if}
       {@html story.Content}
     {/if}
   </p>
