a7fd2b4e76b5139effce715ae87eb1f5b940e1aa
Krunoslav Husak <kruno@husak.me>
2024-02-16 14:51:11 +0100

    Story component code cleanup

--- frontend/src/lib/components/Story.svelte
+++ frontend/src/lib/components/Story.svelte
@@ -17,10 +17,9 @@
   function summarizeStory() {
     buttonSummarizeText = '<span class="loading loading-spinner loading-sm"></span>';
     fetch(`${PUBLIC_API_URL}/stories/${story.ID}/summarize`)
-    .then((response) => response.json())
+      .then((response) => response.json())
       .then((data) => {
         story = data;
-        story.Content = story.Summary;
         storySummarized = true;
       });
   }
@@ -31,14 +30,16 @@
     {#if story === undefined}
       <span class="loading loading-spinner loading-sm"></span>
     {:else}
-      {#if story.Content.length > 800}
-        {#if !storySummarized}
+      {#if !storySummarized}
+        {#if story.Content.length > 800}
           <div class="flex justify-center pb-2">
             <button class="btn btn-sm btn-primary" on:click={summarizeStory}>{@html buttonSummarizeText}</button>
           </div>
         {/if}
+        {@html story.Content}
+      {:else}
+        {@html story.Summary}
       {/if}
-      {@html story.Content}
     {/if}
   </p>
 </div>
