2490123258d1917b04bf34c92802a5539435439b
Krunoslav Husak <kruno@husak.me>
2024-02-23 17:35:07 +0000

    Code formatting on topic page js

--- frontend/src/routes/topics/[topic]/+page.js
+++ frontend/src/routes/topics/[topic]/+page.js
@@ -1,20 +1,20 @@
 import { PUBLIC_API_URL } from '$env/static/public'
 
 export async function load({ fetch, params }) {
-	let [topics, headlines, sources] = await Promise.all([
+  let [topics, headlines, sources] = await Promise.all([
     fetch(`${PUBLIC_API_URL}/topics`).then(res => res.json()),
     fetch(`${PUBLIC_API_URL}/topics/${params.topic}/headlines`).then(res => res.json()),
     fetch(`${PUBLIC_API_URL}/sources`).then(res => res.json())
-	]);
+  ]);
 
   const sourcesMap = sources.reduce((map, obj) => {
     map[obj.id] = obj;
     return map;
   }, {});
 
-	return {
-		topics: topics,
-		selectedTopic: params.topic,
+  return {
+    topics: topics,
+    selectedTopic: params.topic,
     headlines: headlines,
     sources: sourcesMap
   };
