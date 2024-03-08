1d22c8ad452463748670cef1b84e499efed38988
Krunoslav Husak <kruno@husak.me>
2024-03-08 14:46:44 +0100

    Added IsScrapable to Source model

--- backend/app/models/source.go
+++ backend/app/models/source.go
@@ -1,10 +1,11 @@
 package models
 
 type Source struct {
-	ID        uint       `gorm:"primaryKey" json:"id"`
-	Name      string     `json:"name"`
-	TopicID   uint       `gorm:"not null" json:"topic_id"`
-	Headlines []Headline `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+	ID          uint       `gorm:"primaryKey" json:"id"`
+	Name        string     `json:"name"`
+	IsScrapable bool       `json:"is_scrapable"`
+	TopicID     uint       `gorm:"not null" json:"topic_id"`
+	Headlines   []Headline `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
 }
 
 type Sources []Source
--- backend/db/migrate/004_seed_source.go
+++ backend/db/migrate/004_seed_source.go
@@ -7,14 +7,14 @@ import (
 
 func SeedSource(db *raptor.DB) error {
 	sources := models.Sources{
-		models.Source{Name: "klikni.hr", TopicID: 1},
-		models.Source{Name: "MojPortal.hr", TopicID: 1},
-		models.Source{Name: "Radio Daruvar", TopicID: 1},
-		models.Source{Name: "Index.hr", TopicID: 2},
-		models.Source{Name: "N1Info.hr", TopicID: 2},
-		models.Source{Name: "Index.hr", TopicID: 3},
-		models.Source{Name: "N1Info.hr", TopicID: 3},
-		models.Source{Name: "Hacker News", TopicID: 4},
+		models.Source{Name: "klikni.hr", TopicID: 1, IsScrapable: true},
+		models.Source{Name: "MojPortal.hr", TopicID: 1, IsScrapable: true},
+		models.Source{Name: "Radio Daruvar", TopicID: 1, IsScrapable: true},
+		models.Source{Name: "Index.hr", TopicID: 2, IsScrapable: true},
+		models.Source{Name: "N1Info.hr", TopicID: 2, IsScrapable: true},
+		models.Source{Name: "Index.hr", TopicID: 3, IsScrapable: true},
+		models.Source{Name: "N1Info.hr", TopicID: 3, IsScrapable: true},
+		models.Source{Name: "Hacker News", TopicID: 4, IsScrapable: false},
 	}
-	return db.CreateInBatches(&sources, 4).Error
+	return db.CreateInBatches(&sources, 8).Error
 }
