c16c3f8e6aa84e05afed771b227a5e22bd9b2679
Krunoslav Husak <kruno@husak.me>
2024-03-08 14:51:08 +0100

    Disable story display for non-scrapable sources
    This commit disables the functionality of displaying a story if the
    source from which the story is derived is not scrapable. This is to
    ensure that the application does not attempt to display content that it
    cannot properly retrieve and format. Closes #15

--- frontend/src/lib/components/Headline.svelte
+++ frontend/src/lib/components/Headline.svelte
@@ -28,15 +28,17 @@
       {/if}
     </h3>
 
-    <button on:click="{toggleStory}" class="text-gray-300 focus:outline-none">
-      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        {#if showStory}
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
-        {:else}
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-        {/if}
-      </svg>
-    </button>
+    {#if source.is_scrapable}
+      <button on:click="{toggleStory}" class="text-gray-300 focus:outline-none">
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          {#if showStory}
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
+          {:else}
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+          {/if}
+        </svg>
+      </button>
+    {/if}
   </div>
 
   <span class="text-gray-400 mb-4">
