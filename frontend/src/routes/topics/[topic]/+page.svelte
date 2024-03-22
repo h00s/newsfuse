68f2545b9982c8aad19dda5c6b2d77422572cf4e
Krunoslav Husak <kruno@husak.me>
2024-03-22 11:44:33 +0100

    Footer added

--- frontend/src/routes/topics/[topic]/+page.svelte
+++ frontend/src/routes/topics/[topic]/+page.svelte
@@ -5,6 +5,7 @@
   import Navbar from '$lib/components/Navbar.svelte';
   import Tabs from '$lib/components/Tabs.svelte';
   import Headlines from '$lib/components/Headlines.svelte';
+  import Footer from '$lib/components/Footer.svelte';
 
   afterUpdate(async () => {
     setLastAccessedAt(data.selectedTopic);
@@ -16,3 +17,5 @@
 <Navbar />
 <Tabs topics={data.topics} selectedTopic={data.selectedTopic} />
 <Headlines headlines={data.headlines} sources={data.sources} lastAccessedAt={data.lastAccessedAt} />
+<Footer />
+
