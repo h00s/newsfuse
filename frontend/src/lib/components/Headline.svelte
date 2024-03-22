4e7f0e2116ce38539ff92c6b12ec27b50dd4e13b
Krunoslav Husak <kruno@husak.me>
2024-03-22 14:19:10 +0100

    Added sources icons
    This commit finished theme redisign and Closes #22.

--- frontend/src/lib/components/Headline.svelte
+++ frontend/src/lib/components/Headline.svelte
@@ -21,15 +21,25 @@
 
 <div class="rounded overflow-hidden m-2 p-2">
   <div class="flex justify-between items-center">
-    <h3 class="text-gray-900 dark:text-white font-bold">
-      <a class="mb-4" href="{headline.url}">{headline.title}</a>
+    <img class="inline-block rounded-lg pr-3" src="/{source.name}.webp" alt="logo">
+    <div class="flex-1">
+      <h3 class="text-gray-900 dark:text-white font-bold inline">
+        <a class="mb-4" href="{headline.url}">
+          {headline.title}
+        </a>
+      </h3>
+
       {#if newHeadline}
         <span class="text-xs bg-green-500 text-white rounded-full px-2 py-1">NOVO</span>
       {/if}
-    </h3>
+
+      <span class="text-gray-400 pl-1">
+        {humanizeDuration(headline.published_at)}
+      </span>
+    </div>
 
     {#if source.is_scrapable}
-      <button on:click="{toggleStory}" class="text-news-light focus:outline-none">
+      <button on:click="{toggleStory}" class="text-news-light focus:outline-none pl-1">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           {#if showStory}
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
@@ -40,10 +50,6 @@
       </button>
     {/if}
   </div>
-
-  <span class="text-gray-400 mb-4">
-    {humanizeDuration(headline.published_at)} | {source.name}
-  </span>
 
   {#if showStory}
     <Story headlineId={headline.id} />
