8b5368d802814d8bd6df96c3b4100a864c46e3b7
Krunoslav Husak <kruno@husak.me>
2024-03-24 18:06:36 +0100

    Displaying story intro when it's too long

--- frontend/src/lib/components/Story.svelte
+++ frontend/src/lib/components/Story.svelte
@@ -3,42 +3,71 @@
 
   export let headlineId;
   let story;
-  let storySummarized = false;
+  let buttonSummarizeEnabled = false;
+  let buttonContentEnabled = true;
+  let buttonSummarizeText = "Sažmi članak";
+  let displayedStory = "";
 
   if (story === undefined) {
     fetch(`${PUBLIC_API_URL}/headlines/${headlineId}/story`)
       .then((response) => response.json())
       .then((data) => {
         story = data;
+        if (story.content.length > 800) {
+          story.long = true;
+          story.intro = story.content.substring(0, 800).trim() + '...';
+          story.summary === "" ? story.summarized = false : story.summarized = true;
+          buttonContentEnabled = true;
+          buttonSummarizeEnabled = true;
+          displayedStory = story.intro;
+        } else {
+          story.long = false;
+          displayedStory = story.content;
+        }
       });
   }
 
-  function summarizeStory() {
+  function summary() {
     buttonSummarizeText = '<span class="loading loading-spinner loading-sm"></span>';
-    fetch(`${PUBLIC_API_URL}/stories/${story.id}/summarize`)
-      .then((response) => response.json())
-      .then((data) => {
-        story = data;
-        storySummarized = true;
-      });
+    if (!story.summarized) {
+      fetch(`${PUBLIC_API_URL}/stories/${story.id}/summarize`)
+        .then((response) => response.json())
+        .then((data) => {
+          story.summary = data.summary;
+          story.summarized = true;
+          displayedStory = story.summary;
+          buttonSummarizeEnabled = false;
+        });
+    } else {
+      displayedStory = story.summary;
+      buttonSummarizeEnabled = false;
+    }
+  }
+
+  function content() {
+    displayedStory = story.content;
+    buttonContentEnabled = false;
   }
 </script>
 
-<div class="story pt-2 pb-4 text-gray-700 dark:text-gray-300">
+<div class="story pb-2 text-gray-700 dark:text-gray-300">
   {#if story === undefined}
     <span class="loading loading-spinner loading-sm"></span>
   {:else}
-    {#if !storySummarized}
-      {#if story.content.length > 800}
-        <div class="flex justify-center">
-          <button class="btn btn-sm btn-primary text-white" on:click={summarizeStory}>
-            Sažmi članak
+    {@html displayedStory}
+    {#if story.long}
+      <div class="flex justify-center pt-4">
+        {#if buttonContentEnabled}
+          <button class="btn btn-sm btn-primary text-white mr-1" on:click={content}>
+            Cijeli članak
           </button>
-        </div>
-      {/if}
-      {@html story.content}
-    {:else}
-      {@html story.summary}
+        {/if}
+        {#if buttonSummarizeEnabled}
+          <button class="btn btn-sm btn-primary text-white" on:click={summary}>
+            {@html buttonSummarizeText}
+          </button>
+        {/if}
+      </div>
     {/if}
   {/if}
 </div>
