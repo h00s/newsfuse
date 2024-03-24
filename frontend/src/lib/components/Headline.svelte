62f86ebf8e655506b94be760d4a15f6bd386ed93
Krunoslav Husak <kruno@husak.me>
2024-03-24 15:18:20 +0100

    Changed inview library

--- frontend/src/lib/components/Headline.svelte
+++ frontend/src/lib/components/Headline.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from 'svelte';
   import { humanizeDuration } from '$lib/helpers/date'
-  import { useLazyImage as lazyImage } from 'svelte-lazy-image';
+  import { inview } from 'svelte-inview';
   import Story from '$lib/components/Story.svelte'
 
   let showStory = false;
@@ -22,7 +22,7 @@
 
 <div class="rounded overflow-hidden ml-2 p-2">
   <div class="flex justify-between items-center">
-    <img data-src="/sources/{source.name}.webp" class="inline-block rounded-lg pr-3" alt="{source.name} logo" width="32" height="32" use:lazyImage>
+    <img src="/sources/{source.name}.webp" class="inline-block rounded-lg pr-3" alt="{source.name} logo" width="32" height="32" use:inview>
     <div class="flex-1">
       <h3 class="text-gray-900 dark:text-white font-bold inline">
         <a class="mb-4" href="{headline.url}">
