8ce886c9c295b75d42868e52f1e80ec3851e1b49
Krunoslav Husak <kruno@husak.me>
2024-04-10 20:39:20 +0200

    Added footer to search page

--- frontend/src/routes/headlines/search/+page.svelte
+++ frontend/src/routes/headlines/search/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import Navbar from '$lib/components/Navbar.svelte';
   import Search from '$lib/components/Search.svelte';
+  import Footer from '$lib/components/Footer.svelte';
 
   export let data;
 </script>
 
 <Navbar />
 <Search sources={data.sources} />
+<Footer />
