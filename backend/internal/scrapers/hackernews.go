59ddfe9b2fd5fa080ab41c0e7866bf83407ffb4b
Krunoslav Husak <kruno@husak.me>
2024-05-31 13:46:59 +0200

    Rollback hackernews points filter

--- backend/internal/scrapers/hackernews.go
+++ backend/internal/scrapers/hackernews.go
@@ -1,8 +1,6 @@
 package scrapers
 
 import (
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/gocolly/colly/v2"
@@ -22,16 +20,12 @@ func NewHackerNews(h chan (models.Headlines), sourceID uint) *HackerNews {
 	s.ScrapeHeadline("tr[class='athing']", func(e *colly.HTMLElement) {
 		url := s.URL + "item?id=" + e.Attr("id")
 		anchor := e.DOM.Find("span[class='titleline'] > a").First()
-		score := strings.Split(e.DOM.Find("span[class='score']").First().Text(), " ")[0]
-
-		if score, err := strconv.Atoi(score); err == nil && score > 10 {
-			s.AddHeadline(models.Headline{
-				SourceID:    sourceID,
-				Title:       anchor.Text(),
-				URL:         url,
-				PublishedAt: time.Now(),
-			})
-		}
+		s.AddHeadline(models.Headline{
+			SourceID:    sourceID,
+			Title:       anchor.Text(),
+			URL:         url,
+			PublishedAt: time.Now(),
+		})
 	})
 
 	return s
