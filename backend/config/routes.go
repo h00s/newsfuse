2b7664cd23f5d5fc7d3e94ea3d3d2d9ccb1b655c
Krunoslav Husak <kruno@husak.me>
2025-02-18 17:38:18 +0100

    Routes in YAML format

--- backend/config/routes.go
+++ backend/config/routes.go
@@ -6,25 +6,6 @@ import (
 
 func Routes() router.Routes {
 	return router.CollectRoutes(
-		router.Scope("api/v1",
-			router.Scope("topics",
-				router.Get("", "Topics.All"),
-				router.Get(":id/headlines", "Headlines.All"),
-				router.Get(":id/headlines/count", "Headlines.Count"),
-			),
-
-			router.Scope("headlines",
-				router.Get(":id/story", "Stories.Get"),
-				router.Get("search", "Headlines.Search"),
-			),
-
-			router.Scope("sources",
-				router.Get("", "Sources.All"),
-			),
-
-			router.Scope("stories",
-				router.Get(":id/summarize", "Stories.Summarize"),
-			),
-		),
+		router.LoadFromYAML("config/routes.yaml"),
 	)
 }
