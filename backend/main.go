6cfde712803ecb4ad18293a5db494d9316436e3a
Krunoslav Husak <kruno@husak.me>
2025-05-03 13:04:46 +0200

    Reenable logistiq

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-raptor/raptor/v4"
+	"github.com/h00s/newsfuse/app/utils"
 	"github.com/h00s/newsfuse/config"
 	"github.com/h00s/newsfuse/config/components"
 )
@@ -9,12 +10,12 @@ import (
 func main() {
 	app := raptor.New()
 
-	/*logistiq, err := utils.NewLogistiqHandler(app.Core.Resources.Config)
+	logistiq, err := utils.NewLogistiqHandler(app.Core.Resources.Config)
 	if err != nil {
 		app.Core.Resources.Log.Error("Failed to create Logistiq handler", "error", err)
 		return
 	}
-	defer logistiq.Close()*/
+	defer logistiq.Close()
 
 	app.Configure(components.New(app.Core.Resources.Config))
 	app.RegisterRoutes(config.Routes())
