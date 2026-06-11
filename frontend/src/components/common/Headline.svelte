e3320c13b3a4f978233f7814adece7858f7c122c
Krunoslav Husak <kruno@binel.hr>
2026-06-11 16:07:40 +0200

    Toggle story on headline click

--- frontend/src/components/common/Headline.svelte
+++ frontend/src/components/common/Headline.svelte
@@ -1,77 +1,116 @@
 <script>
-  import { onMount, createEventDispatcher } from 'svelte';
-  import { inview } from 'svelte-inview';
-  import { humanizeDuration } from '$lib/utils/date'
-  import Story from '$comp/common/Story.svelte'
-  
-  onMount(() => {
-    newHeadline = lastAccessedAt < new Date(headline.publishedAt).getTime() ? true : false;
-  });
+    import { onMount, createEventDispatcher } from "svelte";
+    import { inview } from "svelte-inview";
+    import { humanizeDuration } from "$lib/utils/date";
+    import Story from "$comp/common/Story.svelte";
 
-  function sendDataToParent() {
-    dispatch('headlineDisplayed', headline.id);
-  }
+    onMount(() => {
+        newHeadline =
+            lastAccessedAt < new Date(headline.publishedAt).getTime()
+                ? true
+                : false;
+    });
 
-  function toggleStory() {
-    showStory = !showStory;
-  }
+    function sendDataToParent() {
+        dispatch("headlineDisplayed", headline.id);
+    }
 
-  function handleHeadlineView({ detail }) {
-    sendDataToParent();
-    sourceLogoInView = true;
-  }
+    function toggleStory() {
+        showStory = !showStory;
+    }
 
-  const headlineInViewOptions = {
-    rootMargin: '50px',
-    unobserveOnEnter: true,
-  };
-  const dispatch = createEventDispatcher();
-  let showStory = false;
-  let sourceLogoInView = false;
-  export let headline;
-  export let source;
-  export let lastAccessedAt;
-  export let newHeadline = false;
+    function handleHeadlineView({ detail }) {
+        sendDataToParent();
+        sourceLogoInView = true;
+    }
+
+    const headlineInViewOptions = {
+        rootMargin: "50px",
+        unobserveOnEnter: true,
+    };
+    const dispatch = createEventDispatcher();
+    let showStory = false;
+    let sourceLogoInView = false;
+    export let headline;
+    export let source;
+    export let lastAccessedAt;
+    export let newHeadline = false;
 </script>
 
-<div class="rounded overflow-hidden ml-2 p-2" use:inview={headlineInViewOptions} on:inview_enter="{handleHeadlineView}">
-  <div class="flex justify-between items-center">
-    {#if sourceLogoInView}
-      <img src="/img/sources/{source.name}.webp" class="inline-block rounded-lg pr-3" alt="{source.name} logo" width="32" height="32">
-    {:else}
-      <div class="inline-block w-8 h-9"></div>
-    {/if}
-    <div class="flex-1">
-      <h3 class="text-gray-900 inline"
-        class:text-white={newHeadline}
-        class:font-bold={newHeadline}
-        class:text-slate-300={!newHeadline}
-        class:font-medium={!newHeadline}
-      >
-        <a class="mb-4" href="{headline.url}">
-          {headline.title}
-        </a>
-      </h3>
+<div
+    class="rounded overflow-hidden ml-2 p-2"
+    use:inview={headlineInViewOptions}
+    on:inview_enter={handleHeadlineView}
+>
+    <div class="flex justify-between items-center">
+        {#if sourceLogoInView}
+            <img
+                src="/img/sources/{source.name}.webp"
+                class="inline-block rounded-lg pr-3"
+                alt="{source.name} logo"
+                width="32"
+                height="32"
+            />
+        {:else}
+            <div class="inline-block w-8 h-9"></div>
+        {/if}
+        <div class="flex-1">
+            <h3
+                class="text-gray-900 inline"
+                class:text-white={newHeadline}
+                class:font-bold={newHeadline}
+                class:text-slate-300={!newHeadline}
+                class:font-medium={!newHeadline}
+            >
+                <a
+                    class="mb-4 cursor-pointer"
+                    href={headline.url}
+                    on:click|preventDefault={source.isScrapable
+                        ? toggleStory
+                        : null}
+                >
+                    {headline.title}
+                </a>
+            </h3>
 
-      <span class="text-gray-400 pl-1">
-        {humanizeDuration(headline.publishedAt)}
-      </span>
+            <span class="text-gray-400 pl-1">
+                {humanizeDuration(headline.publishedAt)}
+            </span>
+        </div>
+
+        {#if source.isScrapable}
+            <button
+                on:click={toggleStory}
+                class="text-news-light focus:outline-none pl-1"
+            >
+                <svg
+                    class="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    {#if showStory}
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M5 15l7-7 7 7"
+                        ></path>
+                    {:else}
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M19 9l-7 7-7-7"
+                        ></path>
+                    {/if}
+                </svg>
+            </button>
+        {/if}
     </div>
 
-    {#if source.isScrapable}
-      <button on:click="{toggleStory}" class="text-news-light focus:outline-none pl-1">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          {#if showStory}
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7"></path>
-          {:else}
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-          {/if}
-        </svg>
-      </button>
+    {#if showStory}
+        <Story headlineUrl={headline.url} headlineId={headline.id} />
     {/if}
-  </div>
-
-  {#if showStory}
-    <Story headlineId={headline.id} />
-  {/if}
 </div>
--- frontend/src/components/common/Story.svelte
+++ frontend/src/components/common/Story.svelte
@@ -1,74 +1,95 @@
 <script>
-  import { onMount } from 'svelte';
-  import { fetchStoryByHeadline, fetchStorySummary } from '$svc/stories';
+    import { onMount } from "svelte";
+    import { fetchStoryByHeadline, fetchStorySummary } from "$svc/stories";
 
-  onMount(() => {
-    if (story === undefined) {
-      fetchStoryByHeadline(headlineId).then((data) => {
-        story = data;
-        if (story.content.length > 650) {
-          story.long = true;
-          story.intro = story.content.substring(0, 600).trim() + '...';
-          story.summary === "" ? story.summarized = false : story.summarized = true;
-          buttonContentEnabled = true;
-          buttonSummarizeEnabled = true;
-          displayedStory = story.intro;
-        } else {
-          story.long = false;
-          displayedStory = story.content;
+    onMount(() => {
+        if (story === undefined) {
+            fetchStoryByHeadline(headlineId).then((data) => {
+                story = data;
+                if (story.content.length > 650) {
+                    story.long = true;
+                    story.intro =
+                        story.content.substring(0, 600).trim() + "...";
+                    story.summary === ""
+                        ? (story.summarized = false)
+                        : (story.summarized = true);
+                    buttonContentEnabled = true;
+                    buttonSummarizeEnabled = true;
+                    displayedStory = story.intro;
+                } else {
+                    story.long = false;
+                    displayedStory = story.content;
+                }
+            });
         }
-      });
+    });
+
+    function summary() {
+        buttonSummarizeText =
+            '<span class="loading loading-spinner loading-sm"></span>';
+        if (!story.summarized) {
+            fetchStorySummary(story.id).then((data) => {
+                story.summary = data.summary;
+                story.summarized = true;
+                displayedStory = story.summary;
+                buttonSummarizeEnabled = false;
+            });
+        } else {
+            displayedStory = story.summary;
+            buttonSummarizeEnabled = false;
+        }
     }
-  });
 
-  function summary() {
-    buttonSummarizeText = '<span class="loading loading-spinner loading-sm"></span>';
-    if (!story.summarized) {
-      fetchStorySummary(story.id).then((data) => {
-        story.summary = data.summary;
-        story.summarized = true;
-        displayedStory = story.summary;
-        buttonSummarizeEnabled = false;
-      });
-    } else {
-      displayedStory = story.summary;
-      buttonSummarizeEnabled = false;
+    function content() {
+        displayedStory = story.content;
+        buttonContentEnabled = false;
     }
-  }
 
-  function content() {
-    displayedStory = story.content;
-    buttonContentEnabled = false;
-  }
-
-  let story;
-  let buttonSummarizeEnabled = false;
-  let buttonContentEnabled = false;
-  let buttonSummarizeText = "Sažmi članak";
-  let displayedStory = "";
-  export let headlineId;
+    let story;
+    let buttonSummarizeEnabled = false;
+    let buttonContentEnabled = false;
+    let buttonSummarizeText = "Sažmi članak";
+    let displayedStory = "";
+    export let headlineId;
+    export let headlineUrl;
 </script>
 
 <div class="story pb-2 text-gray-300">
-  {#if story === undefined}
-    <div class="pt-4">
-      <span class="loading loading-spinner loading-sm"></span>
-    </div>
-  {:else}
-    {@html displayedStory}
-    {#if story.long}
-      <div class="flex justify-center pt-4">
-        {#if buttonContentEnabled}
-          <button class="btn btn-sm btn-primary text-white mr-1" on:click={content}>
-            Cijeli članak
-          </button>
-        {/if}
-        {#if buttonSummarizeEnabled}
-          <button class="btn btn-sm btn-primary text-white" on:click={summary}>
-            {@html buttonSummarizeText}
-          </button>
-        {/if}
-      </div>
+    {#if story === undefined}
+        <div class="pt-4">
+            <span class="loading loading-spinner loading-sm"></span>
+        </div>
+    {:else}
+        {@html displayedStory}
+        <div class="flex justify-center pt-4">
+            {#if headlineUrl}
+                <button
+                    class="btn btn-sm btn-primary text-white mr-1"
+                    on:click={() => window.open(headlineUrl, "_blank")}
+                >
+                    Izvorni članak
+                </button>
+            {/if}
+
+            {#if story.long}
+                {#if buttonContentEnabled}
+                    <button
+                        class="btn btn-sm btn-primary text-white mr-1"
+                        on:click={content}
+                    >
+                        Cijeli članak
+                    </button>
+                {/if}
+
+                {#if buttonSummarizeEnabled}
+                    <button
+                        class="btn btn-sm btn-primary text-white"
+                        on:click={summary}
+                    >
+                        {@html buttonSummarizeText}
+                    </button>
+                {/if}
+            {/if}
+        </div>
     {/if}
-  {/if}
 </div>
